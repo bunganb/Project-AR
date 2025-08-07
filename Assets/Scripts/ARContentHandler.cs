1a9a409f61c62408c0e965cc40880fa946326cd5
bung_anb <bunganabila1005@gmail.com>
2025-08-08 00:39:51 +0700

    add ar UI

--- Assets/Scripts/ARContentHandler.cs
+++ Assets/Scripts/ARContentHandler.cs
@@ -1,17 +1,29 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
 public class ARContentHandler : MonoBehaviour
 {
-    public GameObject infoPanel;
-    public TextMeshProUGUI titleText;
-    public TextMeshProUGUI descriptionText;
+    [Header("UI Panel")]
+    public RectTransform quizPanel;
+    public TextMeshProUGUI questionText;
     public Button playButton;
+    private CanvasGroup quizCanvasGroup;
+
+    [Header("Audio")]
     public AudioSource audioSource;
     private AudioClip currentClip;
     private bool hasPlayed = false;
+
+    [Header("Animation")]
+    public float slideDuration = 0.5f;
+    private bool isAnimating = false;
+
+    private void Awake()
+    {
+        quizCanvasGroup = quizPanel.GetComponent<CanvasGroup>();
+    }
 
     private void Start()
     {
@@ -21,17 +33,31 @@ public class ARContentHandler : MonoBehaviour
         }
         else
         {
-            Debug.Log("PlayButton belum di-assign di Inspector.");
+            Debug.LogWarning("PlayButton belum di-assign di Inspector.");
+        }
+
+        if (quizPanel != null)
+        {
+            quizPanel.offsetMin = new Vector2(0, -600);
+            quizPanel.offsetMax = new Vector2(0, -600);
+            
+            if (quizCanvasGroup != null)
+            {
+                quizCanvasGroup.alpha = 0;
+                quizCanvasGroup.interactable = false;
+                quizCanvasGroup.blocksRaycasts = false;
+            }
         }
     }
 
-
-    public void ShowContent(string title, string description, AudioClip clip)
+    public void ShowContent(string question, AudioClip clip)
     {
-        titleText.text = title;
-        descriptionText.text = description;
+        if (questionText != null)
+        {
+            questionText.text = question;
+        }
+
         currentClip = clip;
-        infoPanel.SetActive(true);
 
         if (!hasPlayed && currentClip != null)
         {
@@ -39,14 +65,25 @@ public class ARContentHandler : MonoBehaviour
             audioSource.Play();
             hasPlayed = true;
         }
-    }
-    public void HideContent()
-    {
-        infoPanel.SetActive(false);
-        audioSource.Stop();
-        hasPlayed = false; 
+
+        if (quizPanel != null && !isAnimating)
+        {
+            StopAllCoroutines();
+            StartCoroutine(SlideIn(quizPanel, slideDuration));
+        }
     }
 
+    public void HideContent()
+    {
+        audioSource.Stop();
+        hasPlayed = false;
+
+        if (quizPanel != null && !isAnimating)
+        {
+            StopAllCoroutines();
+            StartCoroutine(SlideOut(quizPanel, slideDuration));
+        }
+    }
 
     public void PlayAudio()
     {
@@ -55,5 +92,77 @@ public class ARContentHandler : MonoBehaviour
             audioSource.clip = currentClip;
             audioSource.Play();
         }
+    }
+
+    private IEnumerator SlideIn(RectTransform panel, float duration)
+    {
+        isAnimating = true;
+        
+        if (quizCanvasGroup != null)
+        {
+            quizCanvasGroup.interactable = true;
+            quizCanvasGroup.blocksRaycasts = true;
+        }
+
+        Vector2 startMin = new Vector2(0, -600);
+        Vector2 startMax = new Vector2(0, -600);
+        Vector2 end = Vector2.zero;
+
+        float time = 0f;
+        while (time < duration)
+        {
+            panel.offsetMin = Vector2.Lerp(startMin, end, time / duration);
+            panel.offsetMax = Vector2.Lerp(startMax, end, time / duration);
+            if (quizCanvasGroup != null)
+            {
+                quizCanvasGroup.alpha = Mathf.Lerp(0, 1, time / duration);
+            }
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        panel.offsetMin = end;
+        panel.offsetMax = end;
+        if (quizCanvasGroup != null)
+        {
+            quizCanvasGroup.alpha = 1;
+        }
+        isAnimating = false;
+    }
+
+    private IEnumerator SlideOut(RectTransform panel, float duration)
+    {
+        isAnimating = true; 
+        
+        if (quizCanvasGroup != null)
+        {
+            quizCanvasGroup.interactable = false;
+            quizCanvasGroup.blocksRaycasts = false;
+        }
+
+        Vector2 start = Vector2.zero;
+        Vector2 endMin = new Vector2(0, -600);
+        Vector2 endMax = new Vector2(0, -600);
+
+        float time = 0f;
+        while (time < duration)
+        {
+            panel.offsetMin = Vector2.Lerp(start, endMin, time / duration);
+            panel.offsetMax = Vector2.Lerp(start, endMax, time / duration);
+            if (quizCanvasGroup != null)
+            {
+                quizCanvasGroup.alpha = Mathf.Lerp(1, 0, time / duration);
+            }
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        panel.offsetMin = endMin;
+        panel.offsetMax = endMax;
+        if (quizCanvasGroup != null)
+        {
+            quizCanvasGroup.alpha = 0;
+        }
+        isAnimating = false;
     }
 }
--- Assets/Scripts/ImageTracker.cs
+++ Assets/Scripts/ImageTracker.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
@@ -9,8 +10,7 @@ public class MarkerContentData
     public string markerName;
     public GameObject prefab;
     public AudioClip audioClip;
-    public string title;
-    public string description;
+    public string question; 
 }
 
 public class ImageTracker : MonoBehaviour
@@ -22,7 +22,8 @@ public class ImageTracker : MonoBehaviour
     private Dictionary<string, MarkerContentData> markerDictionary;
     private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
     private string currentActiveMarker = null;
-
+    private Coroutine hideUICoroutine;
+    public float hideUIDelay = 2f;
     void Awake()
     {
         trackedImages = GetComponent<ARTrackedImageManager>();
@@ -33,6 +34,7 @@ public class ImageTracker : MonoBehaviour
             markerDictionary[data.markerName] = data;
         }
     }
+    
 
     void OnEnable() => trackedImages.trackedImagesChanged += OnTrackedImagesChanged;
     void OnDisable() => trackedImages.trackedImagesChanged -= OnTrackedImagesChanged;
@@ -43,9 +45,12 @@ public class ImageTracker : MonoBehaviour
         {
             ShowMarkerContent(trackedImage);
         }
+
         foreach (var trackedImage in args.updated)
         {
-            if (trackedImage.trackingState == TrackingState.Tracking)
+            Debug.Log($"[Update] {trackedImage.referenceImage.name} state: {trackedImage.trackingState}");
+        
+            if (trackedImage.trackingState == TrackingState.Tracking || trackedImage.trackingState == TrackingState.Limited)
             {
                 ShowMarkerContent(trackedImage);
             }
@@ -57,7 +62,19 @@ public class ImageTracker : MonoBehaviour
 
         foreach (var trackedImage in args.removed)
         {
+            Debug.Log($"[Removed] {trackedImage.referenceImage.name} was removed.");
             HideMarkerContent(trackedImage);
+        }
+    }
+    void UpdateMarkerContent(ARTrackedImage trackedImage)
+    {
+        string name = trackedImage.referenceImage.name;
+
+        if (spawnedPrefabs.TryGetValue(name, out var instance))
+        {
+            instance.transform.position = trackedImage.transform.position;
+            instance.transform.rotation = trackedImage.transform.rotation;
+            instance.SetActive(true);
         }
     }
 
@@ -82,14 +99,19 @@ public class ImageTracker : MonoBehaviour
                 instance.transform.localScale = Vector3.one * 0.1f;
                 spawnedPrefabs[name] = instance;
             }
+            if (hideUICoroutine != null)
+            {
+                StopCoroutine(hideUICoroutine);
+                hideUICoroutine = null;
+            }
 
-            spawnedPrefabs[name].SetActive(true);
-            contentHandler.ShowContent(data.title, data.description, data.audioClip);
+            spawnedPrefabs[name].SetActive(true); 
+
+            contentHandler.ShowContent(data.question, data.audioClip);
 
             currentActiveMarker = name;
         }
     }
-
 
     void HideMarkerContent(ARTrackedImage trackedImage)
     {
@@ -99,11 +121,27 @@ public class ImageTracker : MonoBehaviour
         {
             spawnedPrefabs[name].SetActive(false);
         }
+
         if (currentActiveMarker == name)
         {
+            if (hideUICoroutine != null)
+            {
+                StopCoroutine(hideUICoroutine);
+            }
+
+            hideUICoroutine = StartCoroutine(DelayedHideUI(name));
+        }
+    }
+    private IEnumerator DelayedHideUI(string markerName)
+    {
+        yield return new WaitForSeconds(hideUIDelay);
+
+        if (currentActiveMarker == markerName)
+        {
             contentHandler.HideContent();
-            currentActiveMarker = null;
+            currentActiveMarker = null; 
         }
     }
 
 }
+ 
